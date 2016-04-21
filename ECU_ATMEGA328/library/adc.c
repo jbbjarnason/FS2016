5d19e30b4448eb4b2c8a678cd29f368c97e3b064
jbbjarnason <jbbjarnason@gmail.com>
2016-04-21 20:38:41 +0000

    Added files to implement Analog to digital conversion in

---
+++ ECU_ATMEGA328/library/adc.c
@@ -0,0 +1,8 @@
+/*
+ * adc.c
+ *
+ *  Created on: 21. apr. 2016
+ *      Author: JonB
+ */
+
+
---
+++ ECU_ATMEGA328/library/adc.h
@@ -0,0 +1,13 @@
+/*
+ * adc.h
+ *
+ *  Created on: 21. apr. 2016
+ *      Author: JonB
+ */
+
+#ifndef LIBRARY_ADC_H_
+#define LIBRARY_ADC_H_
+
+
+
+#endif /* LIBRARY_ADC_H_ */
