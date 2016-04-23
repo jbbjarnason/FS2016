6fae5d1da04d15e561b04fa4cdf00d665a8bfe37
jbbjarnason <jbbjarnason@gmail.com>
2016-04-23 11:04:20 +0000

    ADC ready

--- ECU_ATMEGA328/library/adc.c
+++ ECU_ATMEGA328/library/adc.c
@@ -1,8 +1,62 @@
 /*
- * adc.c
+ * ADC.c
  *
- *  Created on: 21. apr. 2016
+ *  Created on: 1. mar. 2016
  *      Author: JonB
  */
+#include "ADC.h"
 
+// Take first measurement, for the average to work first cycle
+void initSensors()
+{
+	for (int i = 0; i < ADC_CHANNELS; i++)
+		sensor_reading[i] = readADC(i);
+}
+void startADC()
+{
+	// ADC Multiplexer Selection Register
+	// Using Internal AVcc with external capacitor at aref pin,
+	//highest 8 bits are in one register ADCH, analog pin 0 enabled
+	ADMUX = (1 << REFS0) | (1 << ADLAR);
+	// ADC Control and Status Register A
+	// ADC enable, Prescale clock by division of 128, adc interrupt enable, ADC start conversion
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADIE) | (1 << ADSC);
+	// Enable all analog pins
+	DIDR0 = 0;
+	// Initialize vector index counter
+	sensor_index = 0;
+	// Turn on interrupts
+	sei();
+}
+uint8_t readADC(uint8_t pin)
+{
+	// initialize adc
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+	// Make the highest 8 bits in register ADCH (ADLAR)
+	ADMUX = (1 << REFS0) | (1 << ADLAR) | (pin & 0x07);
+	// Start measurement
+	ADCSRA |= (1 << ADSC);
+	// Wait for the ADC conversion to complete
+	while (!(ADCSRA & (1 << ADIF)));
+	// Clear the end of conversion flag
+	ADCSRA |= (1 << ADIF);
+	return ADCH;
+}
+// ADC conversion complete interrupt
+ISR(ADC_vect)
+{
+	PORTD |= (1 << PIND4);
+	prev_sensor_reading = sensor_reading[sensor_index];
+	// 2 point average filter
+	uint16_t tmp = ADCH + prev_sensor_reading;
+	sensor_reading[sensor_index++] = (tmp >> 1); // divide with 2 by shifting right
+	// run through all of the adc channels (pins)
+	if (sensor_index >= ADC_CHANNELS) { // stop reading
+		ADCSRA &= ~(1 << ADIE);
+		sensor_index = 0;
+	} else { // go to next sensor
+		ADMUX = (1 << REFS0) | (1 << ADLAR) | (sensor_index & 0x07);
+		ADCSRA |= (1 << ADSC);
+	}
+}
 
--- ECU_ATMEGA328/library/adc.h
+++ ECU_ATMEGA328/library/adc.h
@@ -1,13 +1,25 @@
 /*
- * adc.h
+ * ADC.h
  *
- *  Created on: 21. apr. 2016
+ *  Created on: 1. mar. 2016
  *      Author: JonB
  */
 
-#ifndef LIBRARY_ADC_H_
-#define LIBRARY_ADC_H_
+#ifndef LIB_ADC_H_
+#define LIB_ADC_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define ADC_CHANNELS 6
+
+void initSensors();
+void startADC();
+uint8_t readADC(uint8_t pin);
+
+uint8_t sensor_reading[ADC_CHANNELS];
+uint8_t prev_sensor_reading;
+uint8_t sensor_index;
 
 
 
-#endif /* LIBRARY_ADC_H_ */
+#endif /* LIB_ADC_H_ */
